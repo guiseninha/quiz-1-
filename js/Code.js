4f27e738eb375465d51def9b40903cf725a86499
guiseninha <107883094+guiseninha@users.noreply.github.com>
2022-12-19 17:08:14 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "DEZEMBRO";
+const accessCode2 = "NATAL";
+const accessCode3 = "RENAS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("Z E B M R O D E", 100,50)
+    fill("lightblue")
+    text("Dica: Qual o mês do natal ", 100,70)
+
+    fill("white")
+    textSize(15)
+    text(" T L A N A", 700,150)
+    fill("lightblue")
+    text("Dica: O papai noel representa que comemoração", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("N E S A R", 100,250)
+    fill("lightblue")
+    text("Dica: Levam o papai noel para os lugares", 100,270)
+
+}
